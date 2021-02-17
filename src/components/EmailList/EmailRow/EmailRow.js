ff1466b933d7771a46fed8c3c577ed8cbdbe1c21
taurusilver7 <prasad20697@gmail.com>
2021-02-17 21:33:09 +0530

    rows updated

--- src/components/EmailList/EmailRow/EmailRow.js
+++ src/components/EmailList/EmailRow/EmailRow.js
@@ -4,10 +4,29 @@ import "./EmailRow.css";
 import { Checkbox, IconButton } from "@material-ui/core";
 import StarBorderIcon from "@material-ui/icons/StarBorder";
 import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import { useHistory } from "react-router-dom";
+import { selectMail } from "../../../features/mailSlice";
+import { useDispatch } from "react-redux";
 
 const EmailRow = ({ id, title, subject, description, time }) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const openMail = () => {
+    dispatch(
+      selectMail({
+        id,
+        title,
+        subject,
+        description,
+        time,
+      })
+    );
+
+    history.push("/mail");
+  };
   return (
-    <div className="emailRow">
+    <div onClick={openMail} className="emailRow">
       <div className="emailRow__options">
         <Checkbox />
         <IconButton>
@@ -20,8 +39,11 @@ const EmailRow = ({ id, title, subject, description, time }) => {
 
       <div className="emailRow__title">{title}</div>
 
-      <div className="emailRow__message"> 
-        <h4>{subject}{" "} <span className="emailRow__description">-{description}</span></h4>
+      <div className="emailRow__message">
+        <h4>
+          {subject}{" "}
+          <span className="emailRow__description">-{description}</span>
+        </h4>
       </div>
 
       <div className="emailRow__time">{time}</div>
